bb78e30dde1a37471620e60078de7d30431e84bb
Marcelo <marcelovqvd@gmail.com>
2023-08-14 12:15:59 -0300

    feat: implement check button

--- src/components/TodoPosts.jsx
+++ src/components/TodoPosts.jsx
@@ -1,17 +1,24 @@
 import trash from '../assets/trash.svg'
 import check from '../assets/check.svg'
+import checkClick from '../assets/checkClick.svg'
 import styles from './TodoPosts.module.css'
+import { useState } from 'react'
 
 export function TodoPosts({ todo, onDeleteTodo }) {
+    const [checkButton, setCheckButton] = useState(false)
 
     function handleDelete() {
         onDeleteTodo(todo)
     }
 
+    function handleCheck() {
+        setCheckButton(!checkButton)
+    }
+
     return (
             <div className={styles.todoWrap}>
                 <div className={styles.todoPosts}>
-                    <img src={check} alt="check" />
+                    {checkButton ? <img src={checkClick} alt="" onClick={handleCheck}/> : <img src={check} alt="check" onClick={handleCheck}/>}                    
                     <p>{todo}</p>
                     <button type='button' onClick={handleDelete}>
                         <img src={trash} alt="trash" />
