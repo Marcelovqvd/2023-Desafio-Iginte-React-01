8feebb7e87c56325dc52b333781c34c9f4d8409a
Marcelo <marcelovqvd@gmail.com>
2023-08-14 23:02:50 -0300

    feat: trash hover styles

--- src/components/TodoPosts.jsx
+++ src/components/TodoPosts.jsx
@@ -1,4 +1,5 @@
 import trash from '../assets/trash.svg'
+import trashHover from '../assets/trashHover.svg'
 import check from '../assets/check.svg'
 import checkClick from '../assets/checkClick.svg'
 import styles from './TodoPosts.module.css'
@@ -6,6 +7,7 @@ import { useState } from 'react'
 
 export function TodoPosts({ todo, onDeleteTodo }) {
     const [checkButton, setCheckButton] = useState(false)
+    const [imgTrash, setImgTrash] = useState(trash)
 
     function handleDelete() {
         onDeleteTodo(todo)
@@ -16,14 +18,14 @@ export function TodoPosts({ todo, onDeleteTodo }) {
     }
 
     return (
-            <div className={styles.todoWrap}>
-                <div className={styles.todoPosts}>
-                    {checkButton ? <img src={checkClick} alt="" onClick={handleCheck}/> : <img src={check} alt="check" onClick={handleCheck}/>}                    
-                    <p>{todo}</p>
-                    <button type='button' onClick={handleDelete}>
-                        <img src={trash} alt="trash" />
-                    </button>                    
-                </div>
+        <div className={styles.todoWrap}>
+            <div className={styles.todoPosts}>
+                {checkButton ? <img src={checkClick} alt="" onClick={handleCheck}/> : <img src={check} alt="check" onClick={handleCheck}/>}                    
+                <p>{todo}</p>
+                <button className={styles.trashButton} onClick={handleDelete}>
+                    <img src={trash} alt="Trash" />
+                </button>                    
             </div>
+        </div>
     )
 }
